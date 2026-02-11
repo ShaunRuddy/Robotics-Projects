cf3fcce2caa80305c4ea360fcbdc96d4118ab2c7
Shaun Ruddy <srudd987@gmail.com>
2026-02-10 19:21:32 -0500

    updated receiver and transmitter

--- electronics/rf_SPI_transceiver/RF_transceiver/src/main.cpp
+++ electronics/rf_SPI_transceiver/RF_transceiver/src/main.cpp
@@ -4,35 +4,24 @@
 #include <RF24.h>
 
 // CE, CSN pins (change if needed)
-RF24 radio(9, 10);
+RF24 radio(7, 8);
 
 // Must match receiver
 const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(115200);
-
+  
   radio.begin();
-  radio.setChannel(76);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setPayloadSize(32);
-
   radio.openWritingPipe(address);
+
+  radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 
-  Serial.println("Transmitter ready");
 }
 
 void loop() {
-  const char message[] = "Hello from TX";
-
-  bool ok = radio.write(message, sizeof(message)); // includes '\0'
-
-  Serial.print("Sent: ");
-  Serial.println(message);
-  Serial.print("Status: ");
-  Serial.println(ok ? "OK" : "FAILED");
-
-  delay(1000);
+    const char text [] = "Hello World";
+    radio.write(&text, sizeof(text));
+    delay(1000);
 }
--- electronics/rf_SPI_transceiver/rf_SPI_receiver/RF_SPI_Receiver/src/main.cpp
+++ electronics/rf_SPI_transceiver/rf_SPI_receiver/RF_SPI_Receiver/src/main.cpp
@@ -2,35 +2,26 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
+//RECEIVER
 // CE, CSN pins (change if needed)
-RF24 radio(9, 10);
+RF24 radio(7, 8);
 
 // Must match transmitter
 const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(115200);
-
   radio.begin();
-  radio.setChannel(76);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setPayloadSize(32);
-
   radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 
-  Serial.println("Receiver ready");
 }
 
 void loop() {
-  if (radio.available()) {
-    char message[32] = {0};   // zero-init = safe string
-
-    radio.read(message, sizeof(message));
-
-    Serial.print("Received: ");
-    Serial.println(message);
+  if(radio.available()){
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
   }
 }
